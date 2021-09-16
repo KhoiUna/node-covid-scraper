a9b766837584528ef4d02a37946cc842e94ddf13
KhoiNguyen <thorwaitson@gmail.com>
2021-09-16 17:50:19 -0500

    add html css page

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const rp = require("request-promise");
 const cheerio = require("cheerio");
-const app = require("express")();
+const express = require("express");
+const app = express();
 
 const url = "https://www.worldometers.info/coronavirus/country/viet-nam/";
 const { PORT = 3000 } = process.env;
 
-app.get("/", (req, res, next) => {
+app.use(express.static("public"));
+
+app.get("/api/data", (req, res, next) => {
   rp(url)
     .then((html) => {
       const $ = cheerio.load(html);
 
       const covidData = {};
+
+      //Scrape last updated date
+      const scrapedDate = $(".label-counter").next().text();
+      covidData.lastUpdated = new Date(scrapedDate.split("Last updated: ")[1]);
+
+      //Scrape covid cases number
       $("div .maincounter-number > span", html).each((i, el) => {
         const value = Number($(el).text().replace(",", ""));
 
@@ -30,9 +39,10 @@ app.get("/", (req, res, next) => {
     })
     .catch((err) => {
       console.error(err);
+      next();
     });
 });
 
 app.listen(PORT, () => {
-  console.log("On " + PORT);
+  console.log("On http://localhost:" + PORT);
 });
